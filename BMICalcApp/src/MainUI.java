3af6d6a39e2157e10f14de439cc2a1809b4e6044
hidari-nut <madevis02@gmail.com>
2023-07-08 01:28:32 +0700

    Interface

--- BMICalcApp/src/MainUI.java
+++ BMICalcApp/src/MainUI.java
@@ -25,17 +25,75 @@ public class MainUI extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        jLabelUser = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jButtonCalculate = new javax.swing.JButton();
+        jButtonChat = new javax.swing.JButton();
+        jButtonStats = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("MS UI Gothic", 1, 14)); // NOI18N
+        jLabel1.setText("Welcome");
+
+        jLabelUser.setFont(new java.awt.Font("MS UI Gothic", 1, 14)); // NOI18N
+        jLabelUser.setText("<user>");
+
+        jLabel2.setFont(new java.awt.Font("Berlin Sans FB Demi", 0, 48)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(51, 153, 255));
+        jLabel2.setText("BMI Calculator");
+
+        jButtonCalculate.setFont(new java.awt.Font("MS UI Gothic", 0, 12)); // NOI18N
+        jButtonCalculate.setIcon(new javax.swing.ImageIcon("E:\\Download\\College\\Distributed Programming\\PROJECT UAS\\BMI-Calculator\\Asset\\CalcLogo12.png")); // NOI18N
+        jButtonCalculate.setText("Calculate");
+
+        jButtonChat.setFont(new java.awt.Font("MS UI Gothic", 0, 12)); // NOI18N
+        jButtonChat.setIcon(new javax.swing.ImageIcon("E:\\Download\\College\\Distributed Programming\\PROJECT UAS\\BMI-Calculator\\Asset\\ChatLogo.png")); // NOI18N
+        jButtonChat.setText("Chat");
+
+        jButtonStats.setFont(new java.awt.Font("MS UI Gothic", 0, 12)); // NOI18N
+        jButtonStats.setIcon(new javax.swing.ImageIcon("E:\\Download\\College\\Distributed Programming\\PROJECT UAS\\BMI-Calculator\\Asset\\StatsLogo.png")); // NOI18N
+        jButtonStats.setText("Stats");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel2)
+                .addGap(147, 147, 147))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(42, 42, 42)
+                .addComponent(jButtonCalculate, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 80, Short.MAX_VALUE)
+                .addComponent(jButtonChat, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(64, 64, 64)
+                .addComponent(jButtonStats, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(39, 39, 39))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(19, 19, 19)
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addComponent(jLabelUser)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(16, 16, 16)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabelUser))
+                .addGap(41, 41, 41)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 48, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButtonCalculate, javax.swing.GroupLayout.PREFERRED_SIZE, 34, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButtonChat)
+                    .addComponent(jButtonStats))
+                .addGap(46, 46, 46))
         );
 
         pack();
@@ -77,5 +135,11 @@ public class MainUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButtonCalculate;
+    private javax.swing.JButton jButtonChat;
+    private javax.swing.JButton jButtonStats;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabelUser;
     // End of variables declaration//GEN-END:variables
 }
