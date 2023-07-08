41f58b5057bdb08d5825c988e3e5180085925336
hidari-nut <madevis02@gmail.com>
2023-07-08 12:20:25 +0700

    Update

--- BMICalcApp/src/EditUI.java
+++ BMICalcApp/src/EditUI.java
@@ -7,12 +7,12 @@
  *
  * @author mputu
  */
-public class RegisterUI extends javax.swing.JFrame {
+public class EditUI extends javax.swing.JFrame {
 
     /**
-     * Creates new form RegisterUI
+     * Creates new form EditUI
      */
-    public RegisterUI() {
+    public EditUI() {
         initComponents();
     }
 
@@ -25,75 +25,58 @@ public class RegisterUI extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jTextFieldPassword = new javax.swing.JTextField();
-        jButtonlogin = new javax.swing.JButton();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel1 = new javax.swing.JLabel();
-        jButtonregister = new javax.swing.JButton();
-        jTextFieldEmail = new javax.swing.JTextField();
-        jTextFieldFName = new javax.swing.JTextField();
-        jLabel5 = new javax.swing.JLabel();
+        jButtonUpdate = new javax.swing.JButton();
         jTextFieldLName = new javax.swing.JTextField();
+        jTextFieldEmail = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        jTextFieldFName = new javax.swing.JTextField();
         jLabel6 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
         jDatePickerBirth = new com.github.lgooddatepicker.components.DatePicker();
+        jTextFieldPassword = new javax.swing.JTextField();
         jLabel8 = new javax.swing.JLabel();
-        jCheckBoxMale = new javax.swing.JCheckBox();
-        jCheckBoxMale1 = new javax.swing.JCheckBox();
-        jLabel7 = new javax.swing.JLabel();
+        jButtonHome = new javax.swing.JButton();
+        jLabelUser = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jLabel2.setText("E-Mail");
-        jLabel2.setFont(new java.awt.Font("MS UI Gothic", 0, 12)); // NOI18N
-
-        jLabel3.setText("Password");
-        jLabel3.setFont(new java.awt.Font("MS UI Gothic", 0, 12)); // NOI18N
-
-        jTextFieldPassword.setFont(new java.awt.Font("MS UI Gothic", 0, 12)); // NOI18N
-
-        jButtonlogin.setText("Log In");
-        jButtonlogin.setFont(new java.awt.Font("MS UI Gothic", 0, 12)); // NOI18N
-
-        jLabel4.setText("Already have an account?");
-        jLabel4.setFont(new java.awt.Font("MS UI Gothic", 0, 12)); // NOI18N
-
-        jLabel1.setText("BMI Calculator");
-        jLabel1.setFont(new java.awt.Font("Berlin Sans FB Demi", 0, 24)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(51, 153, 255));
-
-        jButtonregister.setText("Register");
-        jButtonregister.setFont(new java.awt.Font("MS UI Gothic", 0, 12)); // NOI18N
-        jButtonregister.addActionListener(new java.awt.event.ActionListener() {
+        jButtonUpdate.setFont(new java.awt.Font("MS UI Gothic", 0, 12)); // NOI18N
+        jButtonUpdate.setText("Update");
+        jButtonUpdate.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButtonregisterActionPerformed(evt);
+                jButtonUpdateActionPerformed(evt);
             }
         });
 
+        jTextFieldLName.setFont(new java.awt.Font("MS UI Gothic", 0, 12)); // NOI18N
+
         jTextFieldEmail.setFont(new java.awt.Font("MS UI Gothic", 0, 12)); // NOI18N
+
+        jLabel2.setFont(new java.awt.Font("MS UI Gothic", 0, 12)); // NOI18N
+        jLabel2.setText("E-Mail");
 
         jTextFieldFName.setFont(new java.awt.Font("MS UI Gothic", 0, 12)); // NOI18N
 
-        jLabel5.setText("First Name");
-        jLabel5.setFont(new java.awt.Font("MS UI Gothic", 0, 12)); // NOI18N
-
-        jTextFieldLName.setFont(new java.awt.Font("MS UI Gothic", 0, 12)); // NOI18N
-
-        jLabel6.setText("Last Name");
         jLabel6.setFont(new java.awt.Font("MS UI Gothic", 0, 12)); // NOI18N
+        jLabel6.setText("Last Name");
 
-        jLabel8.setText("Date of Birth");
+        jLabel5.setFont(new java.awt.Font("MS UI Gothic", 0, 12)); // NOI18N
+        jLabel5.setText("First Name");
+
+        jLabel3.setFont(new java.awt.Font("MS UI Gothic", 0, 12)); // NOI18N
+        jLabel3.setText("Password");
+
+        jTextFieldPassword.setFont(new java.awt.Font("MS UI Gothic", 0, 12)); // NOI18N
+
         jLabel8.setFont(new java.awt.Font("MS UI Gothic", 0, 12)); // NOI18N
+        jLabel8.setText("Date of Birth");
 
-        jCheckBoxMale.setFont(new java.awt.Font("MS UI Gothic", 0, 12)); // NOI18N
-        jCheckBoxMale.setText("Female");
+        jButtonHome.setFont(new java.awt.Font("MS UI Gothic", 0, 12)); // NOI18N
+        jButtonHome.setText("Home");
 
-        jCheckBoxMale1.setFont(new java.awt.Font("MS UI Gothic", 0, 12)); // NOI18N
-        jCheckBoxMale1.setText("Male");
-
-        jLabel7.setFont(new java.awt.Font("MS UI Gothic", 0, 12)); // NOI18N
-        jLabel7.setText("Gender");
+        jLabelUser.setFont(new java.awt.Font("MS UI Gothic", 1, 18)); // NOI18N
+        jLabelUser.setText("User Profile");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -104,59 +87,47 @@ public class RegisterUI extends javax.swing.JFrame {
                 .addComponent(jLabel5)
                 .addGap(119, 119, 119))
             .addGroup(layout.createSequentialGroup()
+                .addGap(126, 126, 126)
+                .addComponent(jLabel3)
+                .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
                         .addGap(61, 61, 61)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jTextFieldFName, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(71, 71, 71)
-                                .addComponent(jLabel2))
                             .addComponent(jTextFieldEmail, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(63, 63, 63)
                                 .addComponent(jLabel6))
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                .addGroup(layout.createSequentialGroup()
-                                    .addGap(65, 65, 65)
-                                    .addComponent(jLabel3)
-                                    .addGap(66, 66, 66))
-                                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                    .addComponent(jButtonregister)
-                                    .addGap(48, 48, 48))
-                                .addComponent(jDatePickerBirth, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addComponent(jTextFieldPassword, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGroup(layout.createSequentialGroup()
-                                    .addGap(55, 55, 55)
-                                    .addComponent(jLabel8)))
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                                    .addComponent(jCheckBoxMale)
-                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                    .addComponent(jCheckBoxMale1))
-                                .addComponent(jTextFieldLName, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                            .addComponent(jTextFieldLName, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jDatePickerBirth, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jTextFieldPassword, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(72, 72, 72)
-                        .addComponent(jLabel1)))
+                        .addGap(116, 116, 116)
+                        .addComponent(jLabel8))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(132, 132, 132)
+                        .addComponent(jLabel2)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(9, 48, Short.MAX_VALUE)
+                .addGap(73, 101, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jLabel4)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jButtonlogin)
-                        .addGap(40, 40, 40))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jButtonUpdate, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jButtonHome, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(107, 107, 107))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jLabel7)
-                        .addGap(130, 130, 130))))
+                        .addComponent(jLabelUser)
+                        .addGap(99, 99, 99))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(18, 18, 18)
-                .addComponent(jLabel1)
-                .addGap(18, 18, 18)
+                .addGap(21, 21, 21)
+                .addComponent(jLabelUser)
+                .addGap(29, 29, 29)
                 .addComponent(jLabel2)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jTextFieldEmail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -169,12 +140,6 @@ public class RegisterUI extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jTextFieldLName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jLabel7)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jCheckBoxMale)
-                    .addComponent(jCheckBoxMale1))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jLabel8)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jDatePickerBirth, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -183,21 +148,19 @@ public class RegisterUI extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jTextFieldPassword, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(jButtonregister)
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel4)
-                    .addComponent(jButtonlogin))
+                .addComponent(jButtonUpdate)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 28, Short.MAX_VALUE)
+                .addComponent(jButtonHome)
                 .addGap(17, 17, 17))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButtonregisterActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonregisterActionPerformed
+    private void jButtonUpdateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonUpdateActionPerformed
         // TODO add your handling code here:
         System.out.println(jDatePickerBirth.getDate());
-    }//GEN-LAST:event_jButtonregisterActionPerformed
+    }//GEN-LAST:event_jButtonUpdateActionPerformed
 
     /**
      * @param args the command line arguments
@@ -216,38 +179,34 @@ public class RegisterUI extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(RegisterUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(EditUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(RegisterUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(EditUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(RegisterUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(EditUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(RegisterUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(EditUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new RegisterUI().setVisible(true);
+                new EditUI().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButtonlogin;
-    private javax.swing.JButton jButtonregister;
-    private javax.swing.JCheckBox jCheckBoxMale;
-    private javax.swing.JCheckBox jCheckBoxMale1;
+    private javax.swing.JButton jButtonHome;
+    private javax.swing.JButton jButtonUpdate;
     private com.github.lgooddatepicker.components.DatePicker jDatePickerBirth;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabelUser;
     private javax.swing.JTextField jTextFieldEmail;
     private javax.swing.JTextField jTextFieldFName;
     private javax.swing.JTextField jTextFieldLName;
